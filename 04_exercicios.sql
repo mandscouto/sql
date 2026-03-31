1cd3ee5f551e3b2e8076e3b47205ea090de2e5a6
Amanda Couto <mandscouto@outlook.com>
2026-03-31 16:34:38 -0300

    exercicios

--- 04_exercicios.sql
+++ 04_exercicios.sql
@@ -1,4 +1,31 @@
 -- Qual o dia com maior engajamento de cada aluno que iniciou o curso no dia 01?
+WITH tb_primeiro_dia AS (
+    SELECT DISTINCT idCliente
+    FROM transacoes
+    WHERE substr(DtCriacao,1,10) = '2025-08-25'
+),
+
+tb_dias_curso AS (
+    SELECT DISTINCT idCliente, substr(DtCriacao,1,10) AS diasCurso, IdTransacao
+    FROM transacoes
+    WHERE substr(DtCriacao,1,10) >= '2025-08-25'
+    AND substr(DtCriacao, 1, 10) < '2025-08-30'
+    ORDER by IdTransacao
+),
+
+tb_join AS (
+    SELECT count(DISTINCT t2.IdTransacao) AS contagemTransacoes, t2.diasCurso, t1.idCliente
+    FROM tb_primeiro_dia AS t1
+    INNER JOIN tb_dias_curso AS t2
+    ON t1.idCliente = t2.idCliente
+    GROUP by t1.idCliente
+)
+
+
+SELECT *
+FROM tb_join
+GROUP by idCliente;
+
 
 -- Dentre os clientes de janeiro/2025, quantos assistiram o curso de SQL?
 
